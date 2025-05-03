05302732f5b9125863cddf6a5560f6ce0702f4c8
crowe73 <rowechad73@yahoo.com>
2025-05-02 23:49:37 -0500

    Remove laser from game if it exits viewport screen

--- Scripts/player_laser.gd
+++ Scripts/player_laser.gd
@@ -1,9 +1,14 @@
 extends Area2D
 
-@export var speed: float = 500
+@export var speed: float = 500  # Speed lasers move at.
 
 func _physics_process(delta):
 	move_player_laser_projectile(delta)
 
 func move_player_laser_projectile(delta):
 	position.y -= speed * delta
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited():
+	# If laser exit the game screen, remove from game screen.
+	queue_free()
