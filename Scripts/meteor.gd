b07fc1882206de4f3d49b58d12e0603d4f11a237
crowe73 <rowechad73@yahoo.com>
2025-05-04 18:22:06 -0500

    Shooting meteors
    Write code in meteor.gd to destroy meteor with 10 shots
    Write code in player_laser.gd to remove player's laser from game if it hits meteor

--- Scripts/meteor.gd
+++ Scripts/meteor.gd
@@ -1,13 +1,14 @@
 # Script: meteor.gd.
 extends Area2D
 
-@export var min_speed: float = 0.0          # Minimum meteor speed.
-@export var max_speed: float = 0.0          # maximum meteor speed.
+@export var min_speed: float         = 0.0  # Minimum meteor speed.
+@export var max_speed: float         = 0.0  # maximum meteor speed.
 @export var min_rotation_rate: float = 0.0  # minimum rotation rate.
 @export var max_rotation_rate: float = 0.0  # maximum rotation rate.
+@export var hit_points: int          = 10   # Hits required to destroy meteor.
 
-var speed: float = 0.0                      # speed meteor moves at.
-var rotation_rate: float = 0.0              # rate meteor rotates at.
+var speed: float                     = 0.0  # speed meteor moves at.
+var rotation_rate: float             = 0.0  # rate meteor rotates at.
 
 func _ready():
 	speed = randf_range(min_speed, max_speed)
@@ -16,8 +17,6 @@ func _ready():
 func _physics_process(delta: float):
 	move_meteor(delta)
 	rotate_meteor(delta)
-	
-
 
 func move_meteor(delta):
 	position.y += speed * delta
@@ -25,6 +24,10 @@ func move_meteor(delta):
 func rotate_meteor(delta):
 	rotation_degrees += rotation_rate * delta
 
-
-func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
+func _on_visible_on_screen_notifier_2d_screen_exited():
 	queue_free()
+
+func meteor_damage(amount: int):
+	hit_points -= amount
+	if hit_points <= 0:
+		queue_free()
--- Scripts/player_laser.gd
+++ Scripts/player_laser.gd
@@ -8,7 +8,12 @@ func _physics_process(delta):
 func move_player_laser_projectile(delta):
 	position.y -= speed * delta
 
-
 func _on_visible_on_screen_notifier_2d_screen_exited():
 	# If laser exit the game screen, remove from game screen.
 	queue_free()
+
+func _on_area_entered(area: Area2D):
+	# If player's laser hits meteor, subtract 1 from meteor's hit points.
+	if area.is_in_group("meteor"):
+		area.meteor_damage(1)
+		queue_free()
