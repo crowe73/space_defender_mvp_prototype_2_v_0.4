57909c9ea1d75b41f66af2f145c5203d5ee90ac2
crowe73 <rowechad73@yahoo.com>
2025-05-03 21:30:13 -0500

    Add meteor
    Add meteor sprite to project
    Add collision polygon to meteor Scene
    Write code in meteor.gd script to move and rotate meteor at random speeds
    Add code to meteor.gd script to remove meteor from game when it exits the screen

---
+++ Scripts/meteor.gd
@@ -0,0 +1,30 @@
+# Script: meteor.gd.
+extends Area2D
+
+@export var min_speed: float = 0.0          # Minimum meteor speed.
+@export var max_speed: float = 0.0          # maximum meteor speed.
+@export var min_rotation_rate: float = 0.0  # minimum rotation rate.
+@export var max_rotation_rate: float = 0.0  # maximum rotation rate.
+
+var speed: float = 0.0                      # speed meteor moves at.
+var rotation_rate: float = 0.0              # rate meteor rotates at.
+
+func _ready():
+	speed = randf_range(min_speed, max_speed)
+	rotation_rate = randf_range(min_rotation_rate, max_rotation_rate)
+	
+func _physics_process(delta: float):
+	move_meteor(delta)
+	rotate_meteor(delta)
+	
+
+
+func move_meteor(delta):
+	position.y += speed * delta
+
+func rotate_meteor(delta):
+	rotation_degrees += rotation_rate * delta
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
+	queue_free()
